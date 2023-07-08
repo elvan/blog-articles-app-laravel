ed6480d6a60bdf047ea985ebb75e8cddfe3fddc3
Elvan Hidayat <elvanhd@gmail.com>
2023-07-08 15:47:21 +0700

    setting up article routes

--- routes/web.php
+++ routes/web.php
@@ -2,30 +2,25 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use \App\Http\Controllers\ArticleController;
+use \App\Models\Article;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    $articles = Article::where('user_id', auth()->id())->paginate();
+    return view('dashboard', compact('articles'));
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::resource('/articles', ArticleController::class)->only(['index', 'show']);
+
 Route::middleware('auth')->group(function () {
+    Route::resource('/dashboard/articles', ArticleController::class)->except(['index', 'show']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
